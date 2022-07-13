8e54c3a566c03fa7447ad004f8debc4b4bbef69b
Вадим Сериков <vadimserikov@MacBook-Air-Vadim.local>
2022-07-13 17:26:02 +0300

    Решил задачи

---
+++ задача34/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив ");
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"{count} чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("");
+    Console.WriteLine();
+}
---
+++ задача34/задача36/задача38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива");
+int size = int.Parse(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив ");
+PrintArray(numbers);
+int index = 0;
+
+for (int i = 0; i < numbers.Length; i += 3)
+    index = numbers[i] - index;
+
+Console.WriteLine($"Разница максимального числа в массиве и минимального = {index}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write(" ");
+    Console.WriteLine();
+}
